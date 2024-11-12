c75127b5fbc98202486fcdb8e2900bc835f7419e
Joan Egea <joanegea300@gmail.com>
2024-11-12 12:14:49 +0100

    feat: config.ts url fixed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/harmonixLogoBlack.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://harmonixframework.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
