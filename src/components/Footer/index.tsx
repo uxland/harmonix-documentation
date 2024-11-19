f6e5a41bcd0e70219a8e1fb203ac629c3e21477e
Joan Egea <joanegea300@gmail.com>
2024-11-19 18:22:03 +0100

    footer style updated

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,23 @@ import styles from './styles.module.css';
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <p>Made with ♥ by Harmonix Team | © {new Date().getFullYear()}</p>
+      <p className={styles.colorful}>LINKEDIN</p>
+      <p>REDDIT</p>
+      <p>FACEBOOK</p>
+      <p>TWITTER</p>
+      <p>PRIVACY & POLICY</p>
+      <p>COOKIES POLICY</p>
+      <p className={styles.colorful}>
+        MADE BY{' '}
+        <a 
+          href="https://www.uxland.es" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          UXLAND
+        </a>
+      </p>
     </footer>
   );
 };
