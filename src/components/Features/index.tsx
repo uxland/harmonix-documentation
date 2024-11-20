e8a6d816df71d4521f5814b0c05e9255f194af25
Joan Egea <joanegea300@gmail.com>
2024-11-20 15:51:07 +0100

    features section text style fixed

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -13,18 +13,18 @@ export const Features: React.FC = () => {
         <div>
           <img src="img/icon1.png" alt="benefit1" className={styles.benefitImage} />
         </div>
-        <div>
-          <div>BENEFICI 1 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
-          <div>Equips autònoms i independents</div>
-          <div>Equips independents poden desenvolupar i desplegar els seus frontals <br></br>de manera autònoma amb qualsevol tecnologia. 
-            Elimina les barreres en el teu <br></br>desenvolupament i fes créixer la teva aplicació sense límits, obrint la porta a projectes amb múltiples proveïdors.</div>
+        <div className={styles.benefitContent}>
+          <div className={styles.benefitTitle}>BENEFICI 1 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
+          <div className={styles.benefitSubTitle}>Equips autònoms i independents</div>
+          <div>Equips independents poden desenvolupar i desplegar els seus frontals de manera autònoma amb qualsevol tecnologia. 
+            Elimina les barreres en el teu desenvolupament i fes créixer la teva aplicació sense límits, obrint la porta a projectes amb múltiples proveïdors.</div>
         </div>
       </div>
       <div className={styles.benefit}>
-        <div>
-          <div>BENEFICI 2 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
-          <div>Experiència d'usuari unificada</div>
-          <div>Mitjançant la reutilització de components i la distribució de temes, Harmonix <br></br>permet als equips crear una experiència d'usuari unificada i fluida,<br></br> 
+        <div className={styles.benefitContent}>
+          <div className={styles.benefitTitle}>BENEFICI 2 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
+          <div className={styles.benefitSubTitle}>Experiència d'usuari unificada</div>
+          <div>Mitjançant la reutilització de components i la distribució de temes, Harmonix permet als equips crear una experiència d'usuari unificada i fluida, 
             assegurant una integració sense fissures a través de totes les parts de l'aplicació.</div>
         </div>
         <div>
@@ -35,17 +35,17 @@ export const Features: React.FC = () => {
         <div>
           <img src="img/icon3.png" alt="benefit3" className={styles.benefitImage} />
         </div>
-        <div>
-          <div>BENEFICI 3 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
-          <div>Rendiment, escalabilitat i extensibilitat</div>
+        <div className={styles.benefitContent}>
+          <div className={styles.benefitTitle}>BENEFICI 3 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
+          <div className={styles.benefitSubTitle}>Rendiment, escalabilitat i extensibilitat</div>
           <div>Harmonix utilitza una arquitectura modular de plugins que facilita l'escalabilitat horitzontal i l'extensió amb funcionalitats pròpies i de tercers. </div>
         </div>
       </div>
       <div className={styles.benefit}>
-        <div>
-          <div>BENEFICI 4 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
-          <div>Agnòstic a la tecnologia</div>
-          <div>Les tecnologies canvien ràpidament i mai sabem si la tecnologia triada avui quedarà obsoleta demà. Harmonix és agnòstic a la tecnologia, <br></br>
+        <div className={styles.benefitContent}>
+          <div className={styles.benefitTitle}>BENEFICI 4 <img src="img/vector.svg" alt="vector" className={styles.vectorBenefit} /></div>
+          <div className={styles.benefitSubTitle}>Agnòstic a la tecnologia</div>
+          <div>Les tecnologies canvien ràpidament i mai sabem si la tecnologia triada avui quedarà obsoleta demà. Harmonix és agnòstic a la tecnologia,
           permetent adaptar-te a noves tendències amb rapidesa i evitant el "lock-in" tecnològic.</div>
         </div>
         <div>
