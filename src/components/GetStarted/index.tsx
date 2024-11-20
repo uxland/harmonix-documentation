4d6a6773675e979e532eb84fd9e642d1970e31cd
Joan Egea <joanegea300@gmail.com>
2024-11-20 11:44:25 +0100

    get started buttons updated

--- src/components/GetStarted/index.tsx
+++ src/components/GetStarted/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 export const GetStarted: React.FC = () => {
   return (
@@ -11,7 +12,11 @@ export const GetStarted: React.FC = () => {
         <div className={styles.getStartedSection}>
           <div className={styles.getStartedText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-          <button className={styles.getStartedButton}>GET STARTED</button>
+          <Link
+            className={styles.getStartedButton}
+            to="/docs/concepts/introduccio">
+            GET STARTED
+          </Link>
         </div>
       </div>
     </section>
