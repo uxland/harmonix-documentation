4c0bfe774ed66e703adf74abb32d6e3e014eec8e
Joan Egea <87185751+JE300@users.noreply.github.com>
2024-11-21 13:04:14 +0100

    feat(harmonix-documentation): responsive hero section (#7)
    * first part of the section responsive style added
    
    * style updated
    
    * style updated
    
    * hero section resposive style updated

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -6,16 +6,18 @@ export const HeroSection: React.FC = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.container}>
-        <h1 className={styles.title}>Harmonix</h1>
-        <div className={styles.description}>
-          <div className={styles.header}>The Ultimate<br></br>Multi-Team Micro Frontend Framework</div>
-          <div className={styles.buttons}>
-            <Link
-              className={styles.getStartedButton}
-              to="/docs/concepts/introduccio">
-              GET STARTED
-            </Link>
-            <button className={styles.playgroundButton}>PLAYGROUND</button>
+        <div className={styles.heroWrapper}>
+          <h1 className={styles.title}>Harmonix</h1>
+          <div className={styles.description}>
+            <div className={styles.header}>The Ultimate<br></br>Multi-Team Micro Frontend Framework</div>
+            <div className={styles.buttons}>
+              <Link
+                className={styles.getStartedButton}
+                to="/docs/concepts/introduccio">
+                GET STARTED
+              </Link>
+              <button className={styles.playgroundButton}>PLAYGROUND</button>
+            </div>
           </div>
         </div>
         <div className={styles.arrow}></div>
@@ -25,8 +27,8 @@ export const HeroSection: React.FC = () => {
             <span className={styles.codeText}>Harmonix és un framework JavaScript de microfrontends creat per facilitar i optimitzar el desenvolupament d'SPAs avançades. 
               Facilita la feina d'equips independents, permetent-los treballar de manera eficient, escalable i autònoma en aplicacions complexes.</span>
           </div>
-          <div>
-            <img src="img/harmonixCode.svg" alt="Hero" className={styles.heroImage} />
+          <div className={styles.codeImage}>
+            <img src="img/harmonixCode.png" alt="Hero" className={styles.heroImage} />
           </div>
         </div>
         <div className={styles.line}></div>
